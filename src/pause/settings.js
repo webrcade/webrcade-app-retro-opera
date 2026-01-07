bbfb65fcde60e44d1ca5724110472bef9915f4be
raz0red <raz0red@gmail.com>
2026-01-06 22:38:41 -0800

    bilinear modes

--- src/pause/settings.js
+++ src/pause/settings.js
@@ -31,8 +31,8 @@ export class ThreedoSettingsEditor extends Component {
     const { emulator } = this.props;
 
     const values = {
-      origBilinearMode: emulator.getPrefs().isBilinearEnabled(),
-      bilinearMode: emulator.getPrefs().isBilinearEnabled(),
+      origBilinearMode: emulator.getPrefs().getBilinearMode(),
+      bilinearMode: emulator.getPrefs().getBilinearMode(),
       origScreenSize: emulator.getPrefs().getScreenSize(),
       screenSize: emulator.getPrefs().getScreenSize(),
       controllerCount: emulator.getControllerCount(),
@@ -70,8 +70,7 @@ export class ThreedoSettingsEditor extends Component {
           emulator.setControllerCount(values.controllerCount);
           let updated = false;
           if (values.origBilinearMode !== values.bilinearMode) {
-            emulator.getPrefs().setBilinearEnabled(values.bilinearMode);
-            emulator.updateBilinearFilter();
+            emulator.getPrefs().setBilinearMode(values.bilinearMode);
             updated = true;
           }
           if (values.origScreenSize !== values.screenSize) {
@@ -85,6 +84,7 @@ export class ThreedoSettingsEditor extends Component {
 
           // Set the shader
           await this.shaderService.setShader(values.shaderId);
+          emulator.updateBilinearFilter();
 
           onClose();
         }}
@@ -111,6 +111,7 @@ export class ThreedoSettingsEditor extends Component {
             content: (
               <AppDisplaySettingsTab
                 emulator={emulator}
+                isBilinearMode={true}
                 isActive={tabIndex === 1}
                 setFocusGridComps={setFocusGridComps}
                 values={values}
